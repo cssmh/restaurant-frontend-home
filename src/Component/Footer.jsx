ac9b47795f759e30e79679814481e58237c3bd24
Momen <touristmomen@gmail.com>
2024-11-04 13:21:22 +0600

    footer fully completed

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,12 @@
+import { LuPhoneCall } from "react-icons/lu";
 import bg from "../assets/footer.jpeg";
 import {
   FaFacebookF,
   FaInstagram,
   FaLinkedinIn,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaClock,
   FaTimes,
 } from "react-icons/fa";
 
@@ -17,68 +21,70 @@ const Footer = () => {
         <h2 className="text-xl md:text-3xl uppercase font-semibold mb-8">
           We are ready to give you the best dining experiences
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-12">
           {/* Grid Items */}
           <div className="flex flex-col items-center">
-            <FaTimes className="text-2xl mb-2" />
+            <FaClock className="text-2xl mb-2 text-yellow-400" />
             <h4 className="text-xl font-bold uppercase">Opening Hours</h4>
             <p className="text-sm">Mon - Sun</p>
             <p className="text-sm">9:00 AM to 11:30 PM</p>
           </div>
           <div className="flex flex-col items-center">
-            <FaTimes className="text-2xl mb-2" />
+            <LuPhoneCall oneAlt className="text-2xl mb-2 text-yellow-400" />
             <h4 className="text-xl font-bold uppercase">Let&lsquo;s talk</h4>
             <p className="text-sm">Phone: 1-800-222-4545</p>
             <p className="text-sm">Fax: 1-800-222-4545</p>
           </div>
           <div className="flex flex-col items-center">
-            <FaTimes className="text-2xl mb-2" />
-            <h4 className="text-xl font-bold">BOOK A TABLE</h4>
+            <FaEnvelope className="text-2xl mb-2 text-yellow-400" />
+            <h4 className="text-xl font-bold uppercase">BOOK A TABLE</h4>
             <p className="text-sm">Email: demo@website.com</p>
             <p className="text-sm">Support: support@website.com</p>
           </div>
           <div className="flex flex-col items-center">
-            <FaTimes className="text-2xl mb-2" />
+            <FaMapMarkerAlt className="text-2xl mb-2 text-yellow-400" />
             <h4 className="text-xl font-bold uppercase">Our Address</h4>
-            <p className="text-sm">123 Stree New York City , United</p>
-            <p className="text-sm">States Of America.</p>
+            <p className="text-sm">123 Street, New York City</p>
+            <p className="text-sm">United States Of America.</p>
           </div>
         </div>
-
-        {/* Social Media Icons */}
         <div className="flex justify-center space-x-4 mb-4">
           <a
             href="https://www.facebook.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-xl border-2 border-white rounded-full p-2"
           >
-            <FaFacebookF className="text-xl" />
+            <FaFacebookF />
           </a>
           <a
             href="https://www.instagram.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-xl border-2 border-white rounded-full p-2"
           >
-            <FaInstagram className="text-xl" />
+            <FaInstagram />
           </a>
           <a
             href="https://www.linkedin.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-xl border-2 border-white rounded-full p-2"
           >
-            <FaLinkedinIn className="text-xl" />
+            <FaLinkedinIn />
           </a>
           <a
             href="https://www.twitter.com"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-xl border-2 border-white rounded-full p-2"
           >
-            <FaTimes className="text-xl" />
+            <FaTimes />
           </a>
         </div>
-
-        {/* Copyright Text */}
-        <p className="text-sm">© 2023 Niomax All Rights Reserved</p>
+        <p className="text-sm">
+          © 2023 <span className="text-yellow-400">Niomax</span> All Rights Reserved
+        </p>
       </div>
     </div>
   );
